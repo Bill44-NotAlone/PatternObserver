8ef16ed173a4e4f8c47d660f90ee53b0aec339b9
user <user@412-12.KPC.RU>
2022-01-26 13:32:51 +0300

    Осторожно, ничего не ясно.

---
+++ PatternObserver/PatternObserverLib/ConcreteSubject.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PatternObserverLib.Interfaces;
+
+namespace PatternObserverLib
+{
+    public class ConcreteSubject : ISubject
+    {
+        private string name;
+
+        public void NotifyOdserver()
+        {
+            //
+        }
+
+        public void RegisterObserver()
+        {
+            //
+        }
+
+        public void RemoveObserver()
+        {
+            //
+        }
+
+        public string GetState()
+        {
+            return name;
+        }
+
+        public void SetState(string Name)
+        {
+            name = Name;
+        }
+    }
+}
---
+++ PatternObserver/PatternObserverLib/Interfaces/ISubject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternObserverLib.Interfaces
+{
+    public interface ISubject
+    {
+        void RegisterObserver();
+        void RemoveObserver();
+        void NotifyOdserver();
+    }
+}
